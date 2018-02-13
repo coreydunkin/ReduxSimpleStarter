79c5000f639235ef0e59dad588899815be2828bd
coreydunkin <corey.dunkin@gmail.com>
2018-02-14 00:00:46 +1100

    adding code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyC_iIT9BWq-ENy8w6DUqY6z_CqmskF0t7Y';
+
 // Create new component. This Component should produce some HTML
 
-const App = function() {
+const App = () => {
+
 	return <div>Hi!</div>
 }
 
 // Take this componnent's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
