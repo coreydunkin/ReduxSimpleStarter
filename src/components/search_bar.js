03d9b7c4769538c9abaf6288255eba57cff5b1de
coreydunkin <corey.dunkin@gmail.com>
2018-02-27 00:04:59 +1100

    search bar working

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,20 +9,23 @@ class SearchBar extends React.Component {
 
 	render() {
 		return (
-			<div>	
+			<div className="search-bar">	
 				<input
 					value = {
 						this.state.term
 					}
 					onChange={
-						event => this.setState({ term: event.target.value })
+						event => this.onInputChange(event.target.value)
 					} 
 				/>
 			</div>
 		);
 	}
 
-
+	onInputChange(term) {
+		this.setState({term});
+		this.props.onSearchTermChange(term);
+	}
 }
 
 export default SearchBar;
